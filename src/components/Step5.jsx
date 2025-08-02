410bd53bf1be315e3eee972ab3f986fb613a28b1
Kage Saluone <ksaluone17@gmail.com>
2025-08-01 23:04:00 -0600

    Super basic End page

---
+++ src/components/Step5.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Step5({ func }) {
+    return (
+        <div>
+            <h1>Finished</h1>
+            <button onClick={func}>Go back home</button>
+        </div>
+    )
+}
+
+export default Step5
